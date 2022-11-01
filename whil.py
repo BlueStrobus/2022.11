b11003203bda81b2c2dd3937277065eb5e91a8ee
strobus <116132298+BlueStrobus@users.noreply.github.com>
2022-11-02 08:38:19 +0900

    5

--- whil.py
+++ whil.py
@@ -1,23 +1,27 @@
-#while
-customers="욘두"
-ind=5
-while ind >=1:  #while A:    -> A하는동안 작동
-    print("{},커피가 준비 되었습니다. {}번 남았어요.".format(customer,index)
+# while
+'''customers = "욘두"
+ind = 5
+while ind >= 1:  # while A:    -> A하는동안 작동
     ind -= 1
-    if ind ==0:
-        print("커피는 폐기처분되었습니다.")
+    print("{},커피가 준비 되었습니다. {}번 남았어요.".format(customers, ind))
+
+    if ind == 0:
+        print("커피는 폐기처분되었습니다.")'''
 
 
-'''#무한루프
-visitor="시리"
-index=1
-while true:
-    print("{0}, 커피가 준비 되었습니다. 호출 {1}회.".format(visitor,index))
-    index +=1  #ctrl  +  c누르면 종료
+'''# 무한루프
+visitor = "시리"
+index = 1
+while True:
+    print("{0}, 커피가 준비 되었습니다. 호출 {1}회.".format(visitor, index))
+    index += 1 
+    print(1터미널에 커서두고 ctrl  +  c누르면 종료'''
+
+
+'''# 조건만족
+customer = "로키"
+take = "unnown"
+while customer != take:
+    print("{0}, 커피가 준비 되었습니다.".format(customer))
+    take = input("이름이 어떻게 되세요? >")
 '''
-
-'''#조건만족
-customer="로키
-take="unnown"
-while customer !=take:
-    print'''
