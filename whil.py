654788c4cbb5a3d5b95cf95cc0bac8467b1546c3
strobus <116132298+BlueStrobus@users.noreply.github.com>
2022-11-02 08:26:36 +0900

    why I can't warking
    왜 안되는데ㅔㅔㅔㅔ

---
+++ whil.py
@@ -0,0 +1,23 @@
+#while
+customers="욘두"
+ind=5
+while ind >=1:  #while A:    -> A하는동안 작동
+    print("{},커피가 준비 되었습니다. {}번 남았어요.".format(customer,index)
+    ind -= 1
+    if ind ==0:
+        print("커피는 폐기처분되었습니다.")
+
+
+'''#무한루프
+visitor="시리"
+index=1
+while true:
+    print("{0}, 커피가 준비 되었습니다. 호출 {1}회.".format(visitor,index))
+    index +=1  #ctrl  +  c누르면 종료
+'''
+
+'''#조건만족
+customer="로키
+take="unnown"
+while customer !=take:
+    print'''
