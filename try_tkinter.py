badef8e79f0160768e03f8172f6423d775f75182
strobus <116132298+BlueStrobus@users.noreply.github.com>
2022-11-02 14:43:12 +0900

    Update try_tkinter.py

--- try_tkinter.py
+++ try_tkinter.py
@@ -4,3 +4,14 @@ import tkinter  # Gui모듈 포함시키기
 # newwin = tkinter.Tk()  # 윈도우창 생성
 # newwin.mainloop()  # 위도우가 종료될 때까지 실행
 # ###########기본적인 tkinter 윈도우창만 만들기'''
+
+windy = tkinter.Tk()
+windy.title("코딩공부!!!")  # 윈도우창 이름
+windy.geometry("25*30+2+2")  # 윈도우창 크기
+# 윈도우이름.geometry("너비x높이+x좌표+y좌표")
+windy.resizable(0, 0)  # 아니면 False,False
+# 윈도우이름.resizeable(상하, 좌우)을 이용하여 윈도우 창의 창 크기 조절 가능 여부를 설정할 수 있습니다. True로 설정할 경우 윈도우 창의 크기를 조절할 수 있습니다.
+# Tip : resizeable()을 적용할 때, True=1, False=0을 의미하여 상수를 입력해도 적용이 가능합니다.
+
+
+windy.mainloop()
