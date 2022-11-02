b906f62d57e638c657461c0ccd4525e69a60f237
strobus <116132298+BlueStrobus@users.noreply.github.com>
2022-11-02 09:54:00 +0900

    gggg

--- ContinyeAndBreak.py
+++ ContinyeAndBreak.py
@@ -1,3 +1,13 @@
 #컨티뉴, 브레이크
 
-# 반복문
+# 컨티뉴: 없는거 빼고 계속 진행
+nobook = [7]
+absent = [2, 5]  # 결석
+for student in range(1, 11):  # 1~20
+    if student in absent:
+        continue  # 윗줄에 조건같은거:   아랫줄에 continue 적기
+    elif student in nobook:
+        print("오늘수업 여기까지. {0}은 교무실로 와라".format(student))
+# 브레이크
+        break  # 종료, 끝냄
+    print("{0}, 책을 읽어라".format(student))
