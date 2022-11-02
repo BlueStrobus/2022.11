91511fb6ef3b4fc06227e649bfcdbadd4952564e
strobus <116132298+BlueStrobus@users.noreply.github.com>
2022-11-02 23:01:02 +0900

    대충 알 것 같으니
    과제를 하자

--- try_tkinter.py
+++ try_tkinter.py
@@ -5,7 +5,7 @@ import tkinter  # Gui모듈 포함시키기
 # newwin.mainloop()  # 위도우가 종료될 때까지 실행
 # ###########기본적인 tkinter 윈도우창만 만들기'''
 
-# 이름, 사이즈 지정
+# 이름, 창 크기 지정
 windy = tkinter.Tk()
 windy.title("why only Engglish")  # 윈도우창 이름
 windy.geometry("640x400+100+100")  # 윈도우창 크기
@@ -15,8 +15,23 @@ windy.resizable(1, 1)  # 아니면 False,False
 # Tip : resizeable()을 적용할 때, True=1, False=0(크기조절안됨)을 의미하여 상수를 입력해도 적용이 가능합니다.
 ###########################
 
+# 내용
 lalalabel = tkinter.Label(windy, text="클라이밍수업가라.")
 lalalabel.pack()
 # 위젯이름=tkinter.Label(윈도우창, text="내용")을 사용하여 윈도우 창에 Label 위젯을 설정할 수 있습니다.
 # 위젯이름.pack()을 사용하여 위젯을 배치할 수 있습니다.
+
+#####
+# while True:
+#     lalalabel = tkinter.Label(windy, text=input("말해봐"))
+#     lalalabel.pack()
+# # input한 글이 아랫줄에 적힘 ...반복 #창 끄고나서 입력하면 오류 발생
+#########
+
+# 라벨(변하지 않는 텍스트)
+label = tkinter.Label(windy, text="홀드", width=30,
+                      height=5, fg="blue", relief="solid")
+label.pack()
+
+
 windy.mainloop()
