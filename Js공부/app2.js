e5e909d9481efc8f420f2711339326fb429acbf2
strobus <116132298+BlueStrobus@users.noreply.github.com>
2022-11-08 13:08:15 +0900

    ㅁㄴㅇ

--- Js공부/app2.js
+++ Js공부/app2.js
@@ -2,8 +2,19 @@ const loginForm = document.getElementById("login-form");
 const loginInput = document.querySelector("#login-form input");
 const loginButten = document.querySelector("#login-form rutten");
 
-function onloginBtnClick() {
-    const username = loginInput.value;
+function onLoginSubmit(event) {
+    event.preventDefault(); //새로고침 막음
+}
+
+loginForm.addEventListener("submit", onLoginSubmit) //submit은 기본적으로 새로고침 기능이 있음. 매개변수(파라미트) 따로 지정 안해주면 새로고침 실행함
+console.dir(loginInput);    
+    
+    
+    
+    
+    
+    
+    /*const username = loginInput.value;
     if(username ==="") (
         alert("Please write your name.")
     ) else if (username.length > 15){
@@ -13,4 +24,4 @@ function onloginBtnClick() {
 }
 loginButten.addEventListener("click", onloginBtnClick);
 console.dir(loginInput)
-
+*/
