4520c45dc9f03040882063055bf9760ddfbeeadf
strobus <116132298+BlueStrobus@users.noreply.github.com>
2022-11-09 12:00:38 +0900

    sss

---
+++ Js공부/Momentum/js/clock.js
@@ -0,0 +1,16 @@
+const clock = document.querySelector("h2#clock");
+
+function getClock(){
+    const date = new Date();
+    const hours = String(date.getHours()).padStart(2, "0"); //문자열로 바꿈, 문자열은 pad스타트 쓸 수 있음,
+    //(2, "0") 문자 길이가 2보다 작으면 앞에 0을 붙여라
+    const minutes  = String(date.getMinutes()).padStart(2, "0");
+    const seconds = String(date.getSeconds()).padStart(2, "0");
+    clock.innerText = `${hours}:${minutes}:${seconds}`;
+}
+
+getClock();
+setInterval(getClock, 1000); // 1000 = 1초마다
+
+
+//셋인터널 - 타이머,시간
--- Js공부/Momentum/js/greeting.js
+++ Js공부/Momentum/js/greeting.js
@@ -2,13 +2,14 @@
 // oneventname  마우스 카피...
 const loginForm = document.getElementById("login-form"); //로그인폼
 const loginInput = document.querySelector("#login-form input"); //로그인폼에 인풋
-const greeting = documment.querySelector("#greeting");
+const greeting = document.querySelector("#greeting");
 const HIDDEN_CLASSNAME = "hidden";
 const USERNAME_KEY = "username";
 function paintGreeting(username) {
-    greeting.innerText = 'Hello ${username}';
+    greeting.innerText = `Hello ${username}`;
     greeting.classList.remove(HIDDEN_CLASSNAME);
 }
+
 
 
 //const loginButten = document.querySelector("#login-form butten");
@@ -17,7 +18,7 @@ function onLoginSubmit (event) {
     event.preventDefault();
     const username = loginInput.value;
     localStorage.setItem(USERNAME_KEY, username);
-    paintGreeting(userName);
+    paintGreeting(username);
     loginForm.classList.add(HIDDEN_CLASSNAME);
     console.log(username);
 }
