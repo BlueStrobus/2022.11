09bc6fdd9eddd0ddc0bee0fc730db3a6aecc99ab
strobus <116132298+BlueStrobus@users.noreply.github.com>
2022-11-10 12:13:03 +0900

    html form을 잘못적음

--- Js공부/Momentum/js/todo.js
+++ Js공부/Momentum/js/todo.js
@@ -1,20 +1,20 @@
 const toDoForm = document.getElementById("todo-form");
-const toDoList = document.getElementById("tod-list");
+const toDoList = document.getElementById("todo-list");
 const toDoInput = toDoForm.querySelector("input");
 
 
 function handleToDoSubmit(event) {
     event.preventDefault();
-    //console.log(toDoInput.value); 콘솔확인
+  //  console.log(toDoInput.value); //콘솔확인
     const newToDo = toDoInput.value;
     toDoInput.value = ""
     paintToDO(newToDo);
 } 
 function paintToDO(newToDo) {
     const li =document.createElement("li");
-    const span =document.createElement("spen")
-    li.appendChild(spen);
-    spen.innerText = newToDo;
+    const span =document.createElement("span")
+    li.appendChild(span);
+    span.innerText = newToDo;
     console.log(li);
 }
 toDoForm.addEventListener("submit", handleToDoSubmit);
