952dfc88959951dd9d9b70dbac78d25996cc9eec
strobus <116132298+BlueStrobus@users.noreply.github.com>
2022-11-21 09:01:31 +0900

    123

---
+++ Js공부/유튜브 js/devil/js/devilCording.js
@@ -0,0 +1,38 @@
+//https://www.youtube.com/watch?v=KF6t61yuPCY&t=1026s
+//let a123 = "a";
+//위에 let 썼는데 내용물 바꿀 때 의도적으로 let을 생략하기도 함
+//a123 = "A+";
+
+
+// //자료형
+// const name = "Mike";
+//  //문자형 String "" ''  `고정 ${변수}` 물결 아래에 있는 것
+// const age = 30; //정수형
+
+
+// const myName = `My name is ${name}`; //달러랑 중괄호 사이에 변수
+
+// //console.log(myName);
+
+// const message1 = `나는 ${30+1}살 입니다.`;
+// console.log(message1);
+
+
+// 숫자형
+const age = 30; //숫자형 Number
+const Pi = 3.14;
+
+console.log(1 + 2); // 더하기
+console.log(10 - 3); //빼기
+console.log(3*2); //곱하기
+console.log(6 / 3); //나누기 소숫점까지 나옴
+console.log(7 % 2); //나머지 : 1의 자리까지 나누고 나머지
+console.log(7 / 2 ); //소숫점까지 나옴
+
+3
+7
+6
+2
+1
+3.5
+
